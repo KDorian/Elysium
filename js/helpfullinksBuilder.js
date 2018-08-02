27fa8f9ec711342179204075bd794adb1496a956
KDorian <dorian.kucharski@gmail.com>
2018-08-02 20:56:07 +0200

    Added json - class discords

---
+++ js/helpfullinksBuilder.js
@@ -0,0 +1,122 @@
+var classGuides = {
+  "classGuides": [
+    {
+      "class": "Death Knight",
+      "name": [
+        "Acherus"
+      ],
+      "link": [
+        "https://discord.gg/acherus"
+      ]
+    },
+    {
+      "class": "Demon Hunter",
+      "name": [
+        "The Fel Hammer",
+        "Mardum"
+      ],
+      "link": [
+        "https://discord.gg/zGGkNGC",
+        "https://discord.gg/b6AvnjM"
+      ]
+    },
+    {
+      "class": "Druid",
+      "name": [
+        "Dreamgrove"
+      ],
+      "link": [
+        "https://discord.gg/0dWu0WkuetF87H9H"
+      ]
+    },
+    {
+      "class": "Hunter",
+      "name": [
+        "Trueshot Lodge",
+        "Warcraft Hunter's Union"
+      ],
+      "link": [
+        "https://discord.gg/yqer4BX",
+        "https://discord.gg/G3tYdTG"
+      ]
+    },
+    {
+      "class": "Mage",
+      "name": [
+        "Hall of the Guardians"
+      ],
+      "link": [
+        "https://discord.gg/0gLMHikX2aZ23VdA"
+      ]
+    },
+    {
+      "class": "Monk",
+      "name": [
+        "PeakOfSerenity"
+      ],
+      "link": [
+        "https://discord.gg/0dkfBMAxzTkWj21F"
+      ]
+    },
+    {
+      "class": "Paladin",
+      "name": [
+        "Hammer of Wrath"
+      ],
+      "link": [
+        "https://discord.gg/0dvRDgpa5xZHFfnD"
+      ]
+    },
+    {
+      "class": "Priest",
+      "name": [
+        "Warcraft Priests",
+        "Netherlight Temple",
+        "Focused Will"
+      ],
+      "link": [
+        "https://discord.gg/WarcraftPriests",
+        "https://discord.gg/nExdySC",
+        "https://discord.gg/focusedwill"
+      ]
+    },
+    {
+      "class": "Rogue",
+      "name": [
+        "Ravenholdt"
+      ],
+      "link": [
+        "https://discord.gg/0h08tydxoNhfDVZf"
+      ]
+    },
+    {
+      "class": "Shaman",
+      "name": [
+        "Earthshrine",
+        "Ancestral Guidance"
+      ],
+      "link": [
+        "https://discord.gg/0VcupJEQX0HuE5HH",
+        "https://discord.gg/AcTek6e"
+      ]
+    },
+    {
+      "class": "Warlock",
+      "name": [
+        "Council of the Black Harvest"
+      ],
+      "link": [
+        "https://discord.gg/0onXDymd9Wpc2CEu"
+      ]
+    },
+    {
+      "class": "Warrior",
+      "name": [
+        "Skyhold"
+      ],
+      "link": [
+        "https://discord.gg/0pYY7932lTH4FHW6"
+      ]
+    }
+  ]
+}
